66e3416bc3d560faacf9c0754b08bb75ab940223
Mohammed Tajudeen Onigbanjo <mtonigbanjo@gmail.com>
2024-06-02 22:16:27 -0400

    modified Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,45 +1,87 @@
+import java.io.File;
+
+
 public class Polynomial{
 	double [] pol;
+	int [] powers;
 
 	public  Polynomial(){
-		pol = new double[0];
+		pol = new double[1];
+		powers = new double[1];
 	}
 
-	public Polynomial(double [] a){
-		pol = new double[a.length];
-		for(int i = 0; i < pol.length; i++){
-			pol[i] = a[i];
-		}
-	}
-
-	double  Power(double a,double power){
-		double result = 1;
-		for(int i = 0; i < power; i++){
-			result = result * a;
-		}
-		return result;
-	}
-	public Polynomial add(Polynomial a){
-		double [] temp_pol;
-		if(this.pol.length >= a.pol.length){
-			temp_pol = new double [this.pol.length];
-		}else{
-			temp_pol = new double [a.pol.length];	
-		}
-		for(int i = 0; i < temp_pol.length; i++){
-			if(i < a.pol.length){
-				if(i < this.pol.length){
-					temp_pol[i] += this.pol[i];
-				}
-				temp_pol[i] += a.pol[i];
+	public Polynomial(double [] coef,int [] p){
+		num_inputs = new double[coef.length];
+		x_inputs = new int [p.len]
+		for(int i = 0; i < coef.length(); i ++){
+			if(coef[i] == 0 ){
+						
 			}
 		}
-		
-		 Polynomial result = new Polynomial(temp_pol);
-		return result;
-
 	}
 
+	public Polynomial(File input){
+		Scanner line = new Scanner(input);
+		String equation = line.nextLine();
+		String[] values = equation.split("(?=[+-])");
+		double [] pol_result = new double[values.length()];
+		int [] powers_result = new int[values.length];
+		int counter = 0;
+		for(int i = 0; i < values.length(); i ++ ){
+			String[] temp = values[i].split("x");
+			if(temp[0].contains("-") && temp.length() > 1){
+				String[] temp2 = temp[0].split("-");
+				pol_results[counter] = -Double.parseDouble(temp2[1]);
+				if(temp[1] == ""){
+					powers_result[counter] = 1;
+				}else{
+					powers_result[counter] = Integer.parseInteger(temp[1]);
+				}
+				counter = counter + 1;
+			}else if (!temp[0].contains("-") && temp.length() > 1){
+				String[] temp = values[i].split("x");
+				if(temp[0].contains("+")){
+					String[] temp2 = temp[0].split("+");
+					pol_results[counter] = -Double.parseDouble(temp2[1]);
+					if(temp[1] == ""){
+						powers_result[counter] = 1;
+					}else{
+						powers_result[counter] = Integer.parseInteger(temp[1]);
+					}
+					counter = counter + 1;
+				}
+			}else{
+				if(temp[0].contains("-")){
+					String[] temp2 = temp[0].split("-");
+					pol_results[counter] = -Double.parseDouble(temp2[1]);
+				}else{
+					String[] temp2 = temp[0].split("+");
+					pol_results[counter] = -Double.parseDouble(temp2[1]);
+				}
+				power_results[counter] = 0;
+				counter = counter + 1;
+			}
+		}
+	}
+
+	public Polynomial add(Polynomial a){
+		int matches = 0;
+		for(int i = 0 ; i < a.powers.length(); i++){
+			for(int j = 0; j < this.powers.length(); j ++){
+				if(a.powers[i] == this.powers[i]){
+					matches = matches + 1;
+				}
+			}
+		}
+		int len = a.powers.length() + this.powers.length() - matches;
+		int [] powers = new int[len];
+		Double [] 
+		for(int i = 0; i < a.powers.length(); i++){
+			for()
+
+		}
+	}
+	
 
 	public double evaluate(double a){
 		double result = 0;
@@ -56,6 +98,15 @@ public class Polynomial{
 		}else{
 			return false;
 		}
+	}
+
+	public void saveToFile(String name){
+
+	}
+
+	public Polynomial multiply(Polynomial a){
+		
+
 	}
 	
 }
